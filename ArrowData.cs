25bcd86fab962986ee1754abaf7cea0d41edf68d
YoungSub <suba1001@daum.net>
2025-10-02 12:43:20 +0900

    [class] ArrowData
    화살표와 체크 아스키문자열로 표현

---
+++ ArrowData.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsciiArt
+{
+    internal static class ArrowData
+    {
+        //오른쪽 화살표
+        public static readonly string[] RightArrow = new string[] {
+            "     __  ",
+            " ____\\ \\ ",
+            "|_____\\ \\",
+            "|_____/ /",
+            "     /_/ "
+        };
+        public static readonly string[] LeftArrow = new string[]{
+            "  __     ",
+            " / /____ ",
+            "/ /_____|",
+            "\\ \\_____|",
+            " \\_\\     "
+        };
+        public static readonly string[] UpArrow = new string[] {
+            "  ____  ",
+            " / /\\ \\ ",
+            "/_/ _\\_\\",
+            "  || |  ",
+            "  ||_|  "
+        };
+        public static readonly string[] DownArrow = new string[] {
+            "   _    ",
+            "  | ||  ",
+            " _|_||_ ",
+            "\\ \\  / /",
+            " \\_\\/_/ "
+        };
+
+        //체크 표시
+        public static readonly string[] Check = new string[]{
+            "      __",
+            "__   / /",
+            "\\ \\ / /",
+            " \\ V / ",
+            "  \\_/  "
+        };
+    }
+}
